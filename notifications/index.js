7ffc20abbc64ec475b99493afe06ee9b074763fe
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-03-09 23:29:06 -0500

    Twilio tested

---
+++ notifications/index.js
@@ -0,0 +1,30 @@
+const route = require("express").Router();
+
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const notifySid = process.env.SERVICE_SID;
+
+const client = require("twilio")(accountSid, authToken);
+
+route.post("/", (req, res) => {
+  const phoneNumbers = ["+19195000265", "+18183041213"];
+  const body = "Ice creams are coming!";
+  const service = client.notify.services(notifySid);
+
+  const bindings = phoneNumbers.map(number => {
+    return JSON.stringify({ binding_type: "sms", address: number });
+  });
+  service.notifications
+    .create({
+      toBinding: bindings,
+      body: body
+    })
+    .then(notification => {
+      res.json(notification);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = route;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const notifications = require("./notifications");
 
 const server = express();
 
@@ -10,5 +11,6 @@ server.use(morgan("dev"));
 server.use(express.json());
 
 // Routes
+server.use("/api/notifications", notifications);
 
 module.exports = server;
