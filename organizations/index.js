a11da5c8759868939ad6a4647c72f9ff6227f058
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-03-11 14:39:17 -0400

    POST Organizations

--- organizations/index.js
+++ organizations/index.js
@@ -2,7 +2,7 @@ const route = require("express").Router();
 const db = require("../data/dbConfig");
 const { authenticate } = require("../common/authentication");
 
-route.get("/", (req, res) => {
+route.get("/", authenticate, (req, res) => {
   db("organizations")
     .then(org => {
       res.json(org);
@@ -10,6 +10,44 @@ route.get("/", (req, res) => {
     .catch(error => {
       res.status(500).json({ message: "Server Error" });
     });
+});
+
+route.post("/", authenticate, (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    res.status(422).json({ message: "Organization name is required" });
+  } else {
+    db("organizations")
+      .where({ name })
+      .first()
+      .then(org => {
+        if (org) {
+          res.status(401).json({ message: "Organization already exists" });
+        } else {
+          const user_id = req.decoded.id;
+
+          db("organizations")
+            .insert({ name, user_id })
+            .returning("id")
+            .then(id => {
+              if (id[0]) {
+                db("organizations").then(orgs => {
+                  res.status(201).json(orgs);
+                });
+              } else {
+                res.status(401).json({ message: "Failed to add organization" });
+              }
+            })
+            .catch(error => {
+              res.status(500).json({ message: "Server Error" });
+            });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({ message: "Server Error" });
+      });
+  }
 });
 
 module.exports = route;
