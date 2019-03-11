82969966d2f922aa452626a247481d02722602c7
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-03-11 14:26:35 -0400

    GET for organizations

--- organizations/index.js
+++ organizations/index.js
@@ -1,2 +1,15 @@
 const route = require("express").Router();
 const db = require("../data/dbConfig");
+const { authenticate } = require("../common/authentication");
+
+route.get("/", (req, res) => {
+  db("organizations")
+    .then(org => {
+      res.json(org);
+    })
+    .catch(error => {
+      res.status(500).json({ message: "Server Error" });
+    });
+});
+
+module.exports = route;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const cors = require("cors");
 // const notifications = require("./notifications");
+const organizations = require("./organizations");
 const users = require("./users");
 const server = express();
 
@@ -14,5 +15,6 @@ server.use(cors());
 // Routes
 // server.use("/api/notifications", notifications);
 server.use("/api/users", users);
+server.use("/api/organizations", organizations);
 
 module.exports = server;
